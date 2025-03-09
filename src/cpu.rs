cbd6562d46446c6b5ded64f8a14904ff2891b7ec
Wagomu056 <catchballer@gmail.com>
2025-03-09 14:15:14 +0900

    Add unofficial SBC

--- src/cpu.rs
+++ src/cpu.rs
@@ -434,6 +434,10 @@ impl CPU {
                     let addr = self.get_operand_address(&opcode.mode);
                     self.mem_write(addr, data);
                 }
+                /* SBC */
+                0xeb => {
+                    self.sbc(&opcode.mode);
+                }
                 _ => todo!(),
             }
 
